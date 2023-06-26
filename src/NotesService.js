da14cbf1542a73502a2d07c6b85c7b4fd3b6a1b7
JemmieLim <jemmielim88@gmail.com>
2023-06-26 21:06:53 +0700

    consumer program finished

---
+++ src/NotesService.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-underscore-dangle */
+const { Pool } = require('pg');
+
+class NotesService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async getNotes(userId) {
+    const query = {
+      text: `SELECT notes.* FROM notes
+      LEFT JOIN collaborations
+      ON notes.id = collaborations.note_id
+      WHERE notes.owner = $1 OR collaborations.user_id = $1
+      GROUP BY notes.id`,
+      values: [userId],
+    };
+
+    const result = await this._pool.query(query);
+    return result.rows;
+  }
+}
+
+module.exports = NotesService;
